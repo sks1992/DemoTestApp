153cc3c6dcd9daf809182a13acae33a6a14b2002
Sandeep <sandeepsk15892>
2022-09-29 17:32:45 +0530

    demo

--- lib/view/screens/employee_screen.dart
+++ lib/view/screens/employee_screen.dart
@@ -2,6 +2,8 @@ import 'package:demo_app/view/screens/login_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
+import '../widgets/form_field_text.dart';
+
 class EmployeeScreen extends StatefulWidget {
   const EmployeeScreen({Key? key}) : super(key: key);
 
@@ -10,6 +12,13 @@ class EmployeeScreen extends StatefulWidget {
 }
 
 class _EmployeeScreenState extends State<EmployeeScreen> {
+  final TextEditingController c1 = TextEditingController();
+  final TextEditingController c2 = TextEditingController();
+  final TextEditingController c3 = TextEditingController();
+  final TextEditingController c4 = TextEditingController();
+  final TextEditingController c5 = TextEditingController();
+  final TextEditingController c6 = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,8 +41,70 @@ class _EmployeeScreenState extends State<EmployeeScreen> {
         ],
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [],
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            children: [
+              FormFieldText(
+                controller: c1,
+                title: "Demo",
+                enabled: true,
+              ),
+              FormFieldText(
+                controller: c2,
+                title: "Demo",
+              ),
+              FormFieldText(
+                controller: c3,
+                title: "Demo",
+              ),
+              FormFieldText(
+                controller: c4,
+                title: "Demo",
+              ),
+              FormFieldText(
+                controller: c5,
+                title: "Demo",
+              ),
+              FormFieldText(
+                controller: c6,
+                title: "Demo",
+              ),
+              const SizedBox(height: 15),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  ElevatedButton(
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.white),
+                    onPressed: () {},
+                    child: const Text(
+                      "Demo",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                  ElevatedButton(
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.white),
+                    onPressed: () {},
+                    child: const Text(
+                      "Demo",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                  ElevatedButton(
+                    style:
+                        ElevatedButton.styleFrom(backgroundColor: Colors.white),
+                    onPressed: () {},
+                    child: const Text(
+                      "Demo",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
